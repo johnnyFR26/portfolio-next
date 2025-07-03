f100cf4e168b64e7add933928d957ef0dec59d42
Johnny Fontes Rabelo <149261007+johnnyFR26@users.noreply.github.com>
2025-07-03 20:16:11 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'JohnnyFR26',
+  description: 'Portfolio created with next',
+  generator: 'Johnny Fontes Rabelo',
 }
 
 export default function RootLayout({
